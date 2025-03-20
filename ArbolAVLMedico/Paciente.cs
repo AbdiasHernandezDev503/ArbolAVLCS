1fbed92198bde416f32837c3580960894983aea9
Linares-29 <yancipineda32@gmail.com>
2025-03-20 16:06:39 -0600

    Clase paciente

--- ArbolAVLMedico/Paciente.cs
+++ ArbolAVLMedico/Paciente.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Security.Cryptography.X509Certificates;
-
 public class Paciente
 {
-	public Paciente()
-	{
         public string Nombre { get; set; }
         public string Genero { get; set; }
-        public string TipoSangre {  get; set; }
+    public string TipoSangre { get; set; }
         public string Presion { get; set; }
-}
+
+
+    public Paciente() { }
 }
