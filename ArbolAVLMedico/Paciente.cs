f10a5650b5591ecd5b41dc45edfa9297bba508f2
Linares-29 <yancipineda32@gmail.com>
2025-03-21 05:25:49 -0600

    Constructor lleno en la clase paciente

--- ArbolAVLMedico/Paciente.cs
+++ ArbolAVLMedico/Paciente.cs
@@ -1,10 +1,23 @@
+﻿using System.CodeDom.Compiler;
+
 public class Paciente
+
 {
-        public string Nombre { get; set; }
-        public string Genero { get; set; }
+    public string Nombre { get; set; }
+    public string Genero { get; set; }
     public string TipoSangre { get; set; }
-        public string Presion { get; set; }
+    public string Presion { get; set; }
 
 
-    public Paciente() { }
+    public Paciente(string nombre, string genero, string tipoSangre, string presion) 
+    {
+        Nombre = nombre;
+        Genero = genero;
+        TipoSangre = tipoSangre;
+        Presion = presion;  
+
+    }
 }
+
+
+
